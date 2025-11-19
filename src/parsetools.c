225df1abceeb3e68334072ec746df009befd369f
OwenMastropietro <owen.mastroj@gmail.com>
2025-11-19 01:16:44 -0800

    extended lexer to handle quoted sections, updated tester

--- src/parsetools.c
+++ src/parsetools.c
@@ -1,4 +1,5 @@
 #include "parsetools.h"
+#include "assert.h"
 #include "constants.h"
 #include "types.h"
 #include <ctype.h>
@@ -37,34 +38,99 @@ char *read_line(void) {
     return buffer;
 }
 
+static char *skip_ws(char *p) {
+    while (*p && isspace((unsigned char)*p)) {
+        p++;
+    }
+    return p;
+}
+
+static char *scan_quoted(char *start) {
+    char *p = start;
+    while (*p) {
+        if (*p == '"' && *(p - 1) != '\\') {
+            return p;
+        }
+        p++;
+    }
+    return p;
+}
+
+static char *scan_unquoted(char *start) {
+    char *p = start;
+    while (*p && !isspace((unsigned char)*p)) {
+        p++;
+    }
+    return p;
+}
+
 /**
- * Parse a command line into a list of tokens, separated by whitespace.
- * Returns the number of tokens.
+ * Returns a stream of tokens by splitting cmd line input on whitespace;
+ * preserves quoted sections.
  */
 TokenStream split_cmd_line(char *line) {
     TokenStream ts = {.tokens = NULL, .size = 0};
     if (!line) {
         return ts;
     }
+    assert(!(strlen(line) > MAX_LINE_CHARS));
 
     ts.tokens = calloc(MAX_LINE_TOKENS, sizeof(Token));
     if (!ts.tokens) {
         return ts;
     }
 
-    char *saveptr;
-    char *delimiters = " \t\n"; // whitespace
+    char *p = line;
 
-    int i = 0;
-    char *lex = strtok_r(line, delimiters, &saveptr);
-    while (lex && i < MAX_LINE_TOKENS) {
-        ts.tokens[i].text = lex;
-        ts.tokens[i].type = T_WORD;
-        i++;
+    while (*p) {
+        p = skip_ws(p);
+        if (*p == '\0') {
+            break;
+        }
 
-        lex = strtok_r(NULL, delimiters, &saveptr);
+        char *tok_start;
+        char *tok_end;
+
+        if (*p == '"') {
+            p++;
+            tok_start = p;
+            tok_end = scan_quoted(tok_start);
+
+            if (*tok_end == '"') {
+                *tok_end = '\0';
+                ts.tokens[ts.size++] = (Token){
+                    .type = T_WORD,
+                    .text = tok_start,
+                };
+                p = tok_end + 1;
+            } else {
+                ts.tokens[ts.size++] = (Token){
+                    .type = T_WORD,
+                    .text = tok_start,
+                };
+                break;
+            }
+
+        } else {
+            tok_start = p;
+            tok_end = scan_unquoted(tok_start);
+
+            if (*tok_end) {
+                *tok_end = '\0';
+                ts.tokens[ts.size++] = (Token){
+                    .type = T_WORD,
+                    .text = tok_start,
+                };
+                p = tok_end + 1;
+            } else {
+                ts.tokens[ts.size++] = (Token){
+                    .type = T_WORD,
+                    .text = tok_start,
+                };
+                break;
+            }
+        }
     }
 
-    ts.size = i;
     return ts;
 }
--- tests/unit/test_lexer.c
+++ tests/unit/test_lexer.c
@@ -7,60 +7,90 @@
 
 typedef struct {
     const char *input;
-    const char *expected[MAX_LINE_TOKENS];
-    int expected_count;
+    TokenStream exp;
 } TestCase;
 
 static TestCase tests[] = {
     {
         .input = "",
-        .expected = {NULL},
-        .expected_count = 0,
+        .exp = {.tokens = NULL, .size = 0},
     },
     {
         .input = "     ",
-        .expected = {NULL},
-        .expected_count = 0,
+        .exp = {.tokens = NULL, .size = 0},
     },
     {
         .input = "\t",
-        .expected = {NULL},
-        .expected_count = 0,
+        .exp = {.tokens = NULL, .size = 0},
     },
     {
         .input = "\n",
-        .expected = {NULL},
-        .expected_count = 0,
+        .exp = {.tokens = NULL, .size = 0},
     },
     {
         .input = "hello",
-        .expected = {"hello"},
-        .expected_count = 1,
+        .exp = (TokenStream){
+            .tokens = (Token[]){
+                {.type=T_WORD, .text="hello"},
+            },
+            .size = 1
+        },
     },
     {
         .input = "hello world",
-        .expected = {"hello", "world"},
-        .expected_count = 2,
+        .exp = (TokenStream){
+            .tokens = (Token[]){
+                {.type=T_WORD, .text="hello"},
+                {.type=T_WORD, .text="world"},
+            },
+            .size = 2,
+        },
     },
     {
         .input = "   echo   hello   world ",
-        .expected = {"echo", "hello", "world"},
-        .expected_count = 3,
+        .exp = (TokenStream){
+            .tokens = (Token[]){
+                {.type=T_WORD, .text="echo"},
+                {.type=T_WORD, .text="hello"},
+                {.type=T_WORD, .text="world"},
+            },
+            .size = 3,
+        },
     },
     {
         .input = "ls -l /home/user",
-        .expected = {"ls", "-l", "/home/user"},
-        .expected_count = 3,
+        .exp = (TokenStream){
+            .tokens = (Token[]){
+                {.type=T_WORD, .text="ls"},
+                {.type=T_WORD, .text="-l"},
+                {.type=T_WORD, .text="/home/user"},
+            },
+            .size = 3,
+        },
     },
     {
         .input = "a   b\tc\nd",
-        .expected = {"a", "b", "c", "d"},
-        .expected_count = 4,
+        .exp = (TokenStream){
+            .tokens = (Token[]){
+                {.type=T_WORD, .text="a"},
+                {.type=T_WORD, .text="b"},
+                {.type=T_WORD, .text="c"},
+                {.type=T_WORD, .text="d"},
+            },
+            .size = 4,
+        },
+        
     },
     {
         .input = "echo \"hello world\" !",
-        .expected = {"echo", "\"hello", "world\"", "!"},
-        .expected_count = 4,
+        .exp = (TokenStream){
+            .tokens = (Token[]){
+                {.type=T_WORD, .text="echo"},
+                {.type=T_WORD, .text="hello world"},
+                {.type=T_WORD, .text="!"},
+            },
+            .size = 3,
+        },
     },
 };
 
@@ -75,21 +105,20 @@ int main(void) {
         line[MAX_LINE_CHARS - 1] = '\0';
         assert(strcmp(line, tc->input) == 0);
 
-        TokenStream ts = split_cmd_line(line);
-        int num_tokens = ts.size;
+        TokenStream res = split_cmd_line(line);
+        TokenStream exp = tc->exp;
 
         // I'm keeping ASSERT for learning purposes.
         // Otherwise, it isn't yet informative enough to replace std::assert.
-        ASSERT(num_tokens == tc->expected_count,
-               "expected: %d, got: %d (input: %s)", tc->expected_count,
-               num_tokens, tc->input);
-        for (int t = 0; t < num_tokens; t++) {
-            ASSERT(strcmp(ts.tokens[t].text, tc->expected[t]) == 0,
-                   "expected: %s, got: %s (input: %s)", tc->expected[t],
-                   ts.tokens[t].text, tc->input);
+        ASSERT(res.size == exp.size, "expected: %d, got: %d (input: %s)",
+            exp.size, res.size, tc->input);
+        for (int t = 0; t < res.size; t++) {
+            ASSERT(strcmp(res.tokens[t].text, exp.tokens[t].text) == 0,
+                "expected: %s, got: %s (input: %s)", exp.tokens[t].text,
+                res.tokens[t].text, tc->input);
         }
 
-        free(ts.tokens);
+        free(res.tokens);
     }
 
     printf("passed.\n\n");
